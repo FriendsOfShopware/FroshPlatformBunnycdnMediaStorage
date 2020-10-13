e714931b3e5d1e4418ee136f04b61ddbab73c2de
tinect <s.koenig@tinect.de>
2020-10-13 21:40:46 +0200

    Revert Using autoload of symfony for config

--- src/FroshPlatformBunnycdnMediaStorage.php
+++ src/FroshPlatformBunnycdnMediaStorage.php
@@ -4,14 +4,22 @@ namespace Frosh\BunnycdnMediaStorage;
 
 use Shopware\Core\Framework\Plugin;
 use Shopware\Core\Framework\Plugin\Context\DeactivateContext;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class FroshPlatformBunnycdnMediaStorage extends Plugin
 {
     public function build(ContainerBuilder $container): void
     {
-        $configPath = $this->getPath() . '/Resources/config/services.yml';
+        $configPath = $container->getParameter('kernel.project_dir') . '/var/bunnycdn_config.yml';
         $container->setParameter('frosh_bunnycdn_media_storage.config_path', $configPath);
+
+        if (file_exists($configPath)) {
+            $pathInfo = pathinfo($configPath);
+            $loader = new YamlFileLoader($container, new FileLocator($pathInfo['dirname']));
+            $loader->load($pathInfo['basename']);
+        }
 
         parent::build($container);
     }
