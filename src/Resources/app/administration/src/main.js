f7faa9c2554a426659d1da2e2de7c3bf4b5dc952
tinect <s.koenig@tinect.de>
2023-09-14 00:23:16 +0200

    feat: use generic snippet path without loading them dedicated

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -2,9 +2,3 @@ import './service/bunnycdnApiTestService';
 import './component/bunnycdn-api-test-button';
 import './component/bunnycdn-config-restriction';
 import './component/bunnycdn-alert';
-
-import localeDE from './snippet/de_DE.json';
-import localeEN from './snippet/en_GB.json';
-
-Shopware.Locale.extend('de-DE', localeDE);
-Shopware.Locale.extend('en-GB', localeEN);
